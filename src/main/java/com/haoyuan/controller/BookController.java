8b991b75cff1abd4007ae0d1ae1ddbda251f06e5
59985 <599852099@qq.com>
2023-05-04 13:32:55 +0800

    the first commit

--- src/main/java/com/haoyuan/controller/BookController.java
+++ src/main/java/com/haoyuan/controller/BookController.java
@@ -1,6 +1,7 @@
 package com.haoyuan.controller;
 
 import com.haoyuan.domain.Book;
+import com.haoyuan.domain.R;
 import com.haoyuan.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -13,39 +14,75 @@ public class BookController {
     @Autowired
     private BookService bookService;  //BookService只有一个实现类，这样虽然定义的是接口，但实际指向仍然是具体的实现类
 
+
     @GetMapping
     @ResponseBody
-    public List<Book> getAll(){
-        return bookService.getAll();
+    public R getAll(){
+        List<Book> all = bookService.getAll();
+
+        R res=new R();
+        if(all!=null){
+            res.setFlag(true);
+        }else{
+            res.setFlag(false);
+        }
+        res.setObject(all);
+        return res;
     }
 
     @PostMapping
-    public Boolean save(@RequestBody Book book){
-        return bookService.insert(book);
+    @ResponseBody
+    public R save(@RequestBody Book book){
+        Boolean flag = bookService.insert(book);
+        R res=new R();
+        res.setFlag(flag);
+        return res;
     }
 
     @PutMapping
-    public Boolean update(@RequestBody Book book){
-        return bookService.updateById(book);
+    @ResponseBody
+    public R update(@RequestBody Book book){
+        R res=new R();
+        res.setFlag(bookService.updateById(book));
+        return res;
     }
 
     @DeleteMapping("/{id}")
-    public Boolean delete(@PathVariable Integer id){
-        return bookService.deleteById(id);
+    @ResponseBody
+    public R delete(@PathVariable Integer id){
+        R res=new R();
+        res.setFlag(bookService.deleteById(id));
+        return res;
     }
 
     @GetMapping("/{id}")
-    public Book getById(@PathVariable Integer id){
-        return bookService.getById(id);
+    @ResponseBody
+    public R getById(@PathVariable Integer id){
+        R res=new R();
+        Book book = bookService.getById(id);
+        if(book!=null){
+            res.setFlag(true);
+        }else {
+            res.setFlag(false);
+        }
+        res.setObject(book);
+        return res;
 
     }
 
     //分页查询
     @GetMapping("/{current}/{size}")
     @ResponseBody
-    public List<Book> getByPage(@PathVariable Integer current,@PathVariable Integer size){
+    public R getByPage(@PathVariable Integer current,@PathVariable Integer size){
         List<Book> books = bookService.getByPage(current, size);
-        return books;
+        R res=new R();
+        if(books !=null){
+            res.setFlag(true);
+        }else{
+            res.setFlag(false);
+        }
+        res.setObject(books);
+        return res;
     }
 
 
